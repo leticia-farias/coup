9a4dd05eacddba3564dc9d565190b679487872c6
leticia-farias <leticiafarias.engsoftware@gmail.com>
2026-05-27 17:19:52 -0300

    feat: refatora a lógica de seleção de ações na classe FactoryVersaoInquisidor

--- coup/src/coup/acoes/FactoryVersaoInquisidor.java
+++ coup/src/coup/acoes/FactoryVersaoInquisidor.java
@@ -4,46 +4,33 @@ import coup.factory.IJogoFactory;
 import coup.model.Baralho;
 
 public class FactoryVersaoInquisidor implements IJogoFactory {
-
-	private Acao acao;
 	
 	@Override
 	public Acao acoes(int opcao, Baralho baralho) {
+		Acao acao = null;
 		switch (opcao) {
 		case 1:
-			acao = new Receitar();
-			break;
-
-		case 2:
-			acao = new Ajudar();
-			break;
-
-		case 3:
 			acao = new Ducar();
 			break;
-
-		case 4:
+		case 2:
 			acao = new Capitar();
 			break;
-
+		case 3:
+			acao = new Ducar(); // TODO: Criar a classe Inquisidar posteriormente. Usa Ducar como placeholder.
+			break;
+		case 4:
+			acao = new Ajudar();
+			break;
 		case 5:
-			acao = new Ducar();
+			acao = new Receitar();
 			break;
-
 		case 6:
-			acao = new Ducar(); // inquisidar
-			break;
-
-		case 7:
 			acao = new Assassinar();
 			break;
-
-		case 8:
+		case 7:
 			acao = new Golpear();
 			break;
 		}
-
 		return acao;
 	}
-
 }
